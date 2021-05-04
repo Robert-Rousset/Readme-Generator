27e8836744caed460fc696ba94fe13e5b3913764
Robert Rousset <whyisrob@hotmail.com>
2021-05-04 23:59:41 +0800

    trying to create the writeToFile function, havin trouble, must sleep

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { error } = require('console');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -14,60 +15,65 @@ const questions = [
         name: 'userName',
         message: 'what yo GitHub name?',
     },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'what yo email?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'What is the description?',
-    },
-    {
-        type: 'input',
-        name: 'tableOfContents',
-        message: 'ye',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'how u install?'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'how do use?'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'u got drivers license?',
-        choices: ["Ye i drive", new inquirer.Separator(), "na can't"]
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'how others contribute?'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'how to test?',
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: "any Q's?"
-    }
+    // {
+    //     type: 'input',
+    //     name: 'email',
+    //     message: 'what yo email?',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'description',
+    //     message: 'What is the description?',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'tableOfContents',
+    //     message: 'ye',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'installation',
+    //     message: 'how u install?'
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'usage',
+    //     message: 'how do use?'
+    // },
+    // {
+    //     type: 'list',
+    //     name: 'license',
+    //     message: 'u got drivers license?',
+    //     choices: ["Ye i drive", new inquirer.Separator(), "na can't"]
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'contributing',
+    //     message: 'how others contribute?'
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'tests',
+    //     message: 'how to test?',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'questions',
+    //     message: "any Q's?"
+    // }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(allDaAnswers) {
+    fs.writeFile('README.md', "#" + allDaAnswers.projectTitle, (error) => 
+    error ? console.error(error) : console.log(allDaAnswers) )
+
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
+    inquirer.prompt(questions).then(writeToFile)
 }
 
 // Function call to initialize app
