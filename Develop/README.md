545b6547b5958ff83ef60574b69b8a87a02a15dc
Robert Rousset <whyisrob@hotmail.com>
2021-05-05 18:13:57 +0800

    finished creating/writing readme, just need to add license options and polish a little!

--- Develop/README.md
+++ Develop/README.md
@@ -1,1 +1,29 @@
-#stuff
+# stuff
+## Description
+
+## Table of Contents
+
+[Installation](#installation)
+
+[Usage](#usage)
+
+[License](#license)
+
+[Contribution](#contribution)
+
+[Tests](#tests)
+
+[Questions](#questions)
+
+## Installation
+by installing
+## Usage
+by using
+## License
+Ye i drive
+## How to contribute
+with stuff
+## Tests
+dunno lol
+## Questions
+ If you have any questions please visit my GitHub at https://github.com/robert-rousset or email me at yay@hotmail.com. Thanks to Rob for this amazingly awesome Readme! 
--- Develop/index.js
+++ Develop/index.js
@@ -15,58 +15,48 @@ const questions = [
         name: 'userName',
         message: 'what yo GitHub name?',
     },
-    // {
-    //     type: 'input',
-    //     name: 'email',
-    //     message: 'what yo email?',
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'description',
-    //     message: 'What is the description?',
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'tableOfContents',
-    //     message: 'ye',
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'installation',
-    //     message: 'how u install?'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'usage',
-    //     message: 'how do use?'
-    // },
-    // {
-    //     type: 'list',
-    //     name: 'license',
-    //     message: 'u got drivers license?',
-    //     choices: ["Ye i drive", new inquirer.Separator(), "na can't"]
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'contributing',
-    //     message: 'how others contribute?'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'tests',
-    //     message: 'how to test?',
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'questions',
-    //     message: "any Q's?"
-    // }
+    {
+        type: 'input',
+        name: 'email',
+        message: 'what yo email?',
+    },
+    {
+        type: 'editor',
+        name: 'description',
+        message: 'Write a description of the project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'how u install?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'how do use?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'u got drivers license?',
+        choices: ["Ye i drive", new inquirer.Separator(), "na can't"]
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'how others contribute?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'how to test?',
+    },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(allDaAnswers) {
-    fs.writeFile('README.md', "#" + allDaAnswers.projectTitle, (error) => 
-    error ? console.error(error) : console.log(allDaAnswers) )
+    fs.writeFile('README.md', `# ${allDaAnswers.projectTitle}\n## Description\n${allDaAnswers.description}\n## Table of Contents\n\n[Installation](#installation)\n\n[Usage](#usage)\n\n[License](#license)\n\n[Contribution](#contribution)\n\n[Tests](#tests)\n\n[Questions](#questions)\n\n## Installation\n${allDaAnswers.installation}\n## Usage\n${allDaAnswers.usage}\n## License\n${allDaAnswers.license}\n## How to contribute\n${allDaAnswers.contribution}\n## Tests\n${allDaAnswers.tests}\n## Questions\n If you have any questions please visit my GitHub at https://github.com/${allDaAnswers.userName} or email me at ${allDaAnswers.email}. Thanks to Rob for this amazingly awesome Readme! `, (error) => 
+    error ? console.error(error) : console.log('Great Success') )
 
     
 }
